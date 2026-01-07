ce7a817809d74618a01e21f886d0d63f4d2bce11
Liam Duhame <liamduhame@gmail.com>
2026-01-07 15:35:01 -0500

    Switch from sqlite3 to postgresql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # On Render, set the RENDER_EXTERNAL_HOSTNAME environment variable
+  config.action_mailer.default_url_options = { host: ENV.fetch("RENDER_EXTERNAL_HOSTNAME", "example.com") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
